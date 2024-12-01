1485ed8476d4a0b16c55ec43c0c3d7a8307c37ff
Leon Scroggins III <scroggo@gmail.com>
2024-11-30 20:34:36 -0500

    Fix landing on a wall on the right
    It seems I do not know how to use `is_on_wall` properly. It seems to
    work fine for walls on the left, but when the player lands on the right
    wall they slide down.
    
    We're already tracking when the player hits a wall for animation
    purposes, so go ahead and keep track of whether the player is on a
    surface manually. This prevents sliding down. Remove the function
    `on_surface()`.
    
    In addition, stop using `move_and_slide`, since I do not actually want
    any sliding behavior. Use `move_and_collide` instead, which I think I
    understand a little better after re-reading [the tutorial](https://docs.godotengine.org/en/stable/tutorials/physics/physics_introduction.html#character-collision-response).

--- rust/src/player.rs
+++ rust/src/player.rs
@@ -19,6 +19,7 @@ pub struct Player {
     max_jump_strength: f32,
     #[export]
     fall_acceleration: f32,
+    on_surface: bool,
     base: Base<CharacterBody2D>,
 }
 
@@ -30,6 +31,7 @@ impl ICharacterBody2D for Player {
             target_velocity: Vector2::ZERO,
             max_jump_strength: 20.0,
             fall_acceleration: 75.0,
+            on_surface: false,
             base,
         }
     }
@@ -42,65 +44,58 @@ impl ICharacterBody2D for Player {
     }
 
     fn physics_process(&mut self, delta: f64) {
-        if !self.on_surface() {
+        if !self.on_surface {
             self.target_velocity.y += delta as f32 * self.fall_acceleration;
         }
-        let target_velocity = self.target_velocity;
-        self.base_mut().set_velocity(target_velocity);
-        self.base_mut().move_and_slide();
-
-        for index in 0..self.base().get_slide_collision_count() {
-            let collision = self.base_mut().get_slide_collision(index).unwrap();
-            if collision.get_collider().is_none() {
-                // TODO: Remove. So far I have not seen this log.
-                godot_print!("How was collider {index} none?");
-                continue;
-            }
-
-            let collider = collision.get_collider().unwrap();
-            if collider.cast::<Node>().is_in_group("wall") {
-                // Without this check, we would keep restarting the animation
-                // since it's still colliding while touching.
-                if self.target_velocity.length_squared() > 0.0 {
-                    // Reverse the jump animation to land.
-                    // TODO: Player starts off falling. Since they're not in
-                    // the jumping pose, it looks awkward to play this
-                    // animation. But the real solution may just be to not fall
-                    // like that.
-                    self.sprite()
-                        .play_ex()
-                        .name("jump")
-                        .custom_speed(-3.0)
-                        .from_end(true)
-                        .done();
-                }
-                // Note: Assumes just vertical walls for now.
-                match collision.get_normal() {
-                    Vector2 { x, y: _ } if x > 0.0 => {
-                        self.direction = Direction::Right;
-                        self.base_mut().set_rotation(PI / 2.0);
-                        self.sprite().set_flip_v(false);
-                        self.sprite().set_flip_h(false);
+        let motion = self.target_velocity * delta as f32;
+        let collision_opt = self.base_mut().move_and_collide(motion);
+        if let Some(collision) = collision_opt {
+            if let Some(collider) = collision.get_collider() {
+                if collider.cast::<Node>().is_in_group("wall") {
+                    self.on_surface = true;
+                    // Without this check, we would keep restarting the animation
+                    // since it's still colliding while touching.
+                    if self.target_velocity.length_squared() > 0.0 {
+                        // Reverse the jump animation to land.
+                        // TODO: Player starts off falling. Since they're not in
+                        // the jumping pose, it looks awkward to play this
+                        // animation. But the real solution may just be to not fall
+                        // like that.
+                        self.sprite()
+                            .play_ex()
+                            .name("jump")
+                            .custom_speed(-3.0)
+                            .from_end(true)
+                            .done();
                     }
-                    Vector2 { x, y: _ } if x < 0.0 => {
-                        self.direction = Direction::Left;
-                        self.base_mut().set_rotation(PI / 2.0);
-                        self.sprite().set_flip_v(true);
-                        self.sprite().set_flip_h(false);
-                    }
-                    Vector2 { x: _, y } if y > 0.0 => {
-                        // TODO: Verify this works as expected.
-                        self.sprite().set_flip_v(true);
+                    // Note: Assumes just vertical (and horizontal) walls for now.
+                    match collision.get_normal() {
+                        Vector2 { x, y: _ } if x > 0.0 => {
+                            self.direction = Direction::Right;
+                            self.base_mut().set_rotation(PI / 2.0);
+                            self.sprite().set_flip_v(false);
+                            self.sprite().set_flip_h(false);
+                        }
+                        Vector2 { x, y: _ } if x < 0.0 => {
+                            self.direction = Direction::Left;
+                            self.base_mut().set_rotation(PI / 2.0);
+                            self.sprite().set_flip_v(true);
+                            self.sprite().set_flip_h(false);
+                        }
+                        Vector2 { x: _, y } if y > 0.0 => {
+                            // TODO: Verify this works as expected.
+                            self.sprite().set_flip_v(true);
 
-                        // Jumping sets flip_h properly, so no need to adjust
-                        // here.
+                            // Jumping sets flip_h properly, so no need to adjust
+                            // here.
+                        }
+                        _ => (),
                     }
-                    _ => (),
                 }
             }
         }
 
-        if self.on_surface() {
+        if self.on_surface {
             let input = Input::singleton();
             if input.is_action_pressed("jump") {
                 // Always jump right-side up, facing `Direction`.
@@ -113,6 +108,7 @@ impl ICharacterBody2D for Player {
                 self.target_velocity =
                     Vector2::new(0.0, -self.max_jump_strength / 2.0).rotated(self.get_jump_angle());
                 self.sprite().play_ex().name("jump").done();
+                self.on_surface = false;
             } else {
                 self.target_velocity = Vector2::ZERO;
             }
@@ -122,6 +118,7 @@ impl ICharacterBody2D for Player {
 
 #[godot_api]
 impl Player {
+    // TODO: Handle jumping off the ceiling.
     fn get_jump_angle(&self) -> f32 {
         const PI_OVER_FOUR: f32 = PI / 4.0;
         match self.direction {
@@ -133,13 +130,9 @@ impl Player {
     #[func]
     fn on_blink_timeout(&self) {
         let mut sprite = self.sprite();
-        if self.on_surface() && !sprite.is_playing() {
+        if self.on_surface && !sprite.is_playing() {
             sprite.play_ex().name("blink").done();
         }
-    }
-
-    fn on_surface(&self) -> bool {
-        self.base().is_on_floor() || self.base().is_on_wall() || self.base().is_on_ceiling()
     }
 
     fn sprite(&self) -> Gd<AnimatedSprite2D> {
