b9bf09b44c8da2f792a682d59277f99079447393
Leon Scroggins III <scroggo@gmail.com>
2024-12-02 12:52:25 -0500

    Consistently draw the jump meter below the player
    `player.rs` rotates the entire `CharacterBody2D`, and the `JumpMeter` is
    a child of that node, so it gets rotated, too. Undo the matrix and draw
    using global coordinates so that it always shows below the player.
    
    FIXME: `offset_y` isn't being exported to the editor properly. I'm
    wondering if it's because I already have too many exported fields?
    This is the ninth one, FWIW.

--- rust/src/jump_meter.rs
+++ rust/src/jump_meter.rs
@@ -23,6 +23,13 @@ pub struct JumpMeter {
     inner_style_box: Gd<StyleBoxFlat>,
     #[export]
     fill_color: Color,
+    //#[export] FIXME: This field is not exported to the editor, though I have
+    // tried to fix it with a few guesses:
+    // - move it
+    // - change its type
+    // - replace another export
+    // - close and re-open Godot
+    offset_y: i32,
     base: Base<Control>,
 }
 
@@ -41,6 +48,7 @@ impl IControl for JumpMeter {
             shadow_size: 2,
             inner_style_box: StyleBoxFlat::new_gd(),
             fill_color: Color::GREEN,
+            offset_y: 30,
             base,
         }
     }
@@ -60,15 +68,24 @@ impl IControl for JumpMeter {
     }
 
     fn draw(&mut self) {
+        // `player.rs` rotates the entire `CharacterBody2D` in order to land on
+        // walls and ceilings. Since the `JumpMeter` is a child of that object,
+        // it gets rotated, too. In order to draw it consistently below the
+        // player, undo the transform and use offsets from the global position.
+        let inverse_transform = self.base().get_global_transform().affine_inverse();
+        let global_position = self.base().get_global_position()
+            + Vector2::new(-self.size.x / 2.0, self.offset_y as f32);
+
         let style_box = self.style_box.clone();
-        let rect = Rect2::new(Vector2::ZERO, self.size);
+        let rect = Rect2::new(global_position, self.size);
 
         let inner_style_box = self.inner_style_box.clone();
         let inset = Vector2::new(self.border_width as f32, self.border_width as f32);
         let mut inner_size = self.size - 2.0 * inset;
         inner_size.x *= self.ratio;
-        let inner_rect = Rect2::new(inset, inner_size);
+        let inner_rect = Rect2::new(inset + global_position, inner_size);
 
+        self.base_mut().draw_set_transform_matrix(inverse_transform);
         self.base_mut().draw_style_box(style_box, rect);
         self.base_mut().draw_style_box(inner_style_box, inner_rect);
     }
