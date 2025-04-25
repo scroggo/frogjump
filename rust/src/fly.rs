cc6ee4b9c1ce368c1d6b9d75a69f9bfb21a54039
Leon Scroggins III <scroggo@gmail.com>
2025-04-25 13:06:29 -0400

    Program the flies to travel based on their environment
    Instead of requiring an `AnimationPlayer` for each fly, make a fly
    travel in a straight line until they either collide with the "world" or
    reach the edge of the platform they are above. Set most flies as "self-
    directed", but turn this option off in the bonus level `swarm`.
    
    Use `Direction` to choose the direction the fly is moving. Also flip the
    fly in the editor if it is facing left.
    
    Use a separate `Area2D` to make the fly turn around a little bit sooner.
    Update collision masks. Now `Fly` itself (used by both `fly.tscn` and
    `bonus_fly.tscn`) only detects collisions with the "player" layer, so
    its `on_body_entered` no longer needs to check the groups. Similarly,
    the new `Area2D` only detects the world.
    
    In `swarm`, remove a fly that is hidden behind a frog and was
    immediately eaten upon level start. Make the flies on the right face
    left for symmetry.
    
    In `vertical_level`, move the bonus fly so it hovers along a branch.
    Adjust another fly so that its new flight path hints at the non-bonus
    side of the vine.
    
    TODO: Share scene tree between `fly.tscn` and `bonus_fly.tscn`?

--- rust/src/fly.rs
+++ rust/src/fly.rs
@@ -1,34 +1,24 @@
-use godot::classes::{AnimatedSprite2D, Area2D, IArea2D};
+use crate::direction::Direction;
+
+use godot::classes::{AnimatedSprite2D, Area2D, Engine, IArea2D, RayCast2D};
 use godot::global::{randf, randi_range};
 use godot::prelude::*;
 
-use crate::player::Player;
-
 #[derive(Clone)]
 enum State {
-    Reset,               // Search for an idle route
-    Idle,                // Fly between two points, determined in `Reset`
-    Fleeing(Gd<Player>), // Fleeing a player trying to eat them
-}
-
-// Path for the fly to travel
-#[derive(Default)]
-struct Path {
-    // These are in local coordinates.
-    start: Vector2,
-    end: Vector2,
-    // If true, the fly is traveling back to the start
-    reverse: bool,
+    Callibrating,        // Looking for a surface to hover over.
+    Hovering,            // Hovering over a surface.
 }
 
 #[derive(GodotClass)]
-#[class(base=Area2D)]
+#[class(base=Area2D, tool)]
 struct Fly {
     /// When `true`, code will drive the fly's motion.
     #[export]
     self_directed: bool,
     state: State,
-    path: Path,
+    #[export]
+    direction: Direction,
     #[export]
     movement_speed: f32,
     base: Base<Area2D>,
@@ -38,17 +28,33 @@ struct Fly {
 impl IArea2D for Fly {
     fn init(base: Base<Area2D>) -> Self {
         Self {
-            self_directed: false,
-            state: State::Reset,
-            path: Path::default(),
-            movement_speed: 20.0,
+            self_directed: true,
+            state: State::Callibrating,
+            direction: Direction::Right,
+            movement_speed: 100.0,
             base,
         }
     }
 
     fn ready(&mut self) {
+        self.set_facing();
+        if Engine::singleton().is_editor_hint() {
+            return;
+        }
+
         let gd = Gd::from_instance_id(self.base().instance_id());
-        self.base_mut().signals().body_entered().connect_obj(&gd, Self::on_body_entered);
+        self.base_mut()
+            .signals()
+            .body_entered()
+            .connect_obj(&gd, Self::on_body_entered);
+
+        let mut avoidance_area = self
+            .base()
+            .get_node_as::<Area2D>("CollisionAvoidanceArea2D");
+        avoidance_area
+            .signals()
+            .body_entered()
+            .connect_obj(&gd, Self::on_collision_avoidance);
 
         let mut sprite = self
             .base()
@@ -59,47 +65,38 @@ impl IArea2D for Fly {
             .unwrap();
         let frame = randi_range(0, (frame_count - 1).into()) as i32;
         sprite.set_frame_and_progress(frame, randf() as f32);
+
+        if !self.self_directed {
+            self.ray_cast().set_enabled(false);
+        }
     }
 
-    fn physics_process(&mut self, delta: f32) {
+    fn physics_process(&mut self, delta: f64) {
+        if Engine::singleton().is_editor_hint() {
+            return;
+        }
         if !self.self_directed {
             return;
         }
 
+        let ray_cast = self.ray_cast();
         match self.state.clone() {
-            State::Reset => {
-                // TODO: Pick a better path to fly between
-                let start = self.base().get_position();
-                let end = start + Vector2 { x: 64.0, y: 0.0 };
-                self.path = Path {
-                    start,
-                    end,
-                    reverse: false,
-                };
-                self.state = State::Idle;
-            }
-            State::Idle => {
-                let dest = if self.path.reverse {
-                    &self.path.start
-                } else {
-                    &self.path.end
-                };
-                let position = self.base().get_position();
-                let movement_direction = *dest - position;
-                let movement_delta = self.movement_speed * delta;
-                let new_position = position.move_toward(*dest, movement_delta);
-                if new_position == *dest {
-                    self.path.reverse = !self.path.reverse;
+            State::Callibrating => {
+                // During the first couple of frames, the ray cast may not
+                // succeed. Continue to hover in the faced direction.
+                self.move_internal(delta);
+
+                if ray_cast.is_colliding() {
+                    self.state = State::Hovering;
                 }
-                let x_scale = if movement_direction.x >= 0.0 {
-                    1.0
-                } else {
-                    -1.0
-                };
-                self.base_mut().set_scale(Vector2 { x: x_scale, y: 1.0 });
-                self.base_mut().set_position(new_position);
             }
-            State::Fleeing(_player) => todo!(),
+            State::Hovering => {
+                if !ray_cast.is_colliding() {
+                    // Flew off the edge.
+                    self.turn_around();
+                }
+                self.move_internal(delta);
+            }
         }
     }
 }
@@ -110,10 +107,51 @@ impl Fly {
     fn eaten();
 
     #[func]
-    fn on_body_entered(&mut self, body: Gd<Node2D>) {
-        if body.is_in_group("player") {
-            self.base_mut().queue_free();
-            self.base_mut().emit_signal("eaten", &[]);
+    fn on_body_entered(&mut self, _body: Gd<Node2D>) {
+        // This `Area2D` only detects the player.
+        self.base_mut().queue_free();
+        self.base_mut().emit_signal("eaten", &[]);
+    }
+
+    #[func]
+    fn on_collision_avoidance(&mut self, _body: Gd<Node2D>) {
+        // This callback is only called by collisions with the world.
+        self.turn_around();
+    }
+
+    fn direction_multipler(&self) -> f32 {
+        if self.direction == Direction::Right {
+            1.0
+        } else {
+            -1.0
         }
+    }
+
+    fn set_facing(&mut self) {
+        let x_scale = self.direction_multipler();
+        self.base_mut().set_scale(Vector2 { x: x_scale, y: 1.0 });
+    }
+
+    fn turn_around(&mut self) {
+        self.direction = !self.direction;
+        // Switch back to `Callibrating` to avoid switching back and forth.
+        self.state = State::Callibrating;
+        self.set_facing();
+    }
+
+    fn move_internal(&mut self, delta: f64) {
+        let movement_delta = self.movement_speed as f64 * delta * self.direction_multipler() as f64;
+        let position = self.base().get_position();
+        self.base_mut().set_position(
+            position
+                + Vector2 {
+                    x: movement_delta as f32,
+                    y: 0.0,
+                },
+        );
+    }
+
+    fn ray_cast(&self) -> Gd<RayCast2D> {
+        self.base().get_node_as::<RayCast2D>("RayCast2D")
     }
 }
